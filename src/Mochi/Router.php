91308c206a56e20db6fa1cd56f5ef3204937446a
juan beresiarte <juanberesiarte@gmail.com>
2024-08-24 23:15:46 -0300

    Add route method in Router class

--- src/Mochi/Router.php
+++ src/Mochi/Router.php
@@ -6,40 +6,44 @@ class Router
 {
     protected static array $routes = [];
 
+    public static function route(HttpMethods $method, string $uri, callable $callback){
+        self::$routes[$method->value][$uri] = $callback;
+    }
+
     public static function get(string $uri, callable $action)
     {
-        self::$routes[HttpMethod::GET][$uri] = $action;
+        self::$routes[HttpMethods::GET->value][$uri] = $action;
     }
 
     public static function post(string $uri, callable $action)
     {
-        self::$routes[HttpMethod::POST][$uri] = $action;
+        self::$routes[HttpMethods::POST->value][$uri] = $action;
     }
 
     public static function put(string $uri, callable $action)
     {
-        self::$routes[HttpMethod::PUT][$uri] = $action;
+        self::$routes[HttpMethods::PUT->value][$uri] = $action;
     }
 
     public static function delete(string $uri, callable $action)
     {
-        self::$routes[HttpMethod::DELETE][$uri] = $action;
+        self::$routes[HttpMethods::DELETE->value][$uri] = $action;
     }
 
     public static function patch(string $uri, callable $action)
     {
-        self::$routes[HttpMethod::PATCH][$uri] = $action;
+        self::$routes[HttpMethods::PATCH->value][$uri] = $action;
     }
 
-    public static function resolve()
+    public static function resolve(null | string $uri = null, null | string $method = null): callable
     {
-        $method = $_SERVER["REQUEST_METHOD"];
-        $uri = $_SERVER["REQUEST_URI"];
+        $method ??= $_SERVER["REQUEST_METHOD"];
+        $uri ??= $_SERVER["REQUEST_URI"];
 
         $action = self::$routes[$method][$uri] ?? null;
 
         if ($action) {
-            return call_user_func($action);
+            return $action;
         }
 
         throw new HttpNotFoundException();
